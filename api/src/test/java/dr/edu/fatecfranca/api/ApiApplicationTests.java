f5f977d24b1cea672fa7cf1d4fc9a1c81ec3851d
VitimFlash <victorlazarini1@gmail.com>
2026-08-10 18:44:06 +0000

    (10/08) Criação do projeto e primeiro controller

---
+++ api/src/test/java/dr/edu/fatecfranca/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dr.edu.fatecfranca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
