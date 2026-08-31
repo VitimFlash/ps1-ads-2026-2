41e9da559af1324eb4612a079977790d5340ed4c
VitimFlash <victorlazarini1@gmail.com>
2026-08-31 16:00:23 +0000

    (31/08) Início da operação do primeiro controller

---
+++ api/src/main/java/dr/edu/fatecfranca/api/repositories/CustomerRepository.java
@@ -0,0 +1,12 @@
+package dr.edu.fatecfranca.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import dr.edu.fatecfranca.api.entities.Customer;
+
+
+public interface CustomerRepository
+      extends JpaRepository<Customer, Long> {
+}
+
